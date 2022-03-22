57b9377bffb261decb33ea7be6126f0304900c9a
Spartak-javohir <dellspartak0@gmail.com>
2022-03-22 19:14:47 +0500

    buging

--- neftuyushma/src/components/burger/burger.jsx
+++ neftuyushma/src/components/burger/burger.jsx
@@ -94,7 +94,7 @@ const StyledBurger = styled.button`
   div {
     width: 2rem;
     height: 0.25rem;
-    background: ${({ open }) => (open ? "#0D0C1D" : "#EFFFFA")};
+    background: ${({ open }) => (open ? "#0D0C1D" : "#0D0C1D")};
     border-radius: 10px;
     transition: all 0.3s linear;
     position: relative;
