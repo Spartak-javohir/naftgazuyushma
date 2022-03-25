fadad45e1583ad6a9676eb80996a9cef086e63c0
Spartak-javohir <dellspartak0@gmail.com>
2022-03-26 02:25:19 +0500

    change

--- neftuyushma/src/components/maps/map.jsx
+++ neftuyushma/src/components/maps/map.jsx
@@ -3,8 +3,11 @@ import "./map.css";
 const map = () => {
   return (
     <>
-      <section>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1462.7025489545817!2d69.2453955869419!3d41.32203393043937!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8b745c326a07%3A0x1bc5d68d16dfa46!2sTKTI%2036!5e0!3m2!1sru!2s!4v1648242943466!5m2!1sru!2s"></iframe>
+      <section className="section_map">
+        <iframe
+          className="iframe"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1462.7025489545817!2d69.2453955869419!3d41.32203393043937!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8b745c326a07%3A0x1bc5d68d16dfa46!2sTKTI%2036!5e0!3m2!1sru!2s!4v1648242943466!5m2!1sru!2s"
+        ></iframe>
       </section>
     </>
   );
