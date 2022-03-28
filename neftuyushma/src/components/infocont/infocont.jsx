0d6cb5d22de435c1b472311e682dddb4e7cb58c4
Spartak-javohir <dellspartak0@gmail.com>
2022-03-28 23:15:43 +0500

    change

---
+++ neftuyushma/src/components/infocont/infocont.jsx
@@ -0,0 +1,19 @@
+import "./infocont.css";
+
+const infocont = () => {
+  return (
+    <>
+      <section className="section_info">
+        <div className="container">
+          <h2>Kontakt ma'lumotlar</h2>
+          <p className="p_info">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut et
+            assumenda harum iure quis provident voluptatum dolore a facilis non.
+          </p>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default infocont;
--- neftuyushma/src/pages/contacts/contact.jsx
+++ neftuyushma/src/pages/contacts/contact.jsx
@@ -1,10 +1,13 @@
 import "./contact.css";
 import Navbar from "../../components/navbar/navbar";
 import Map from "../../components/maps/map";
+import Infocont from "../../components/infocont/infocont";
+
 const contact = () => {
   return (
     <>
       <Navbar />
+      <Infocont />
       <Map />
     </>
   );
