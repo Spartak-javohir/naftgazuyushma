37115f4368eb924c93043c97d50369dc938ea21c
Spartak-javohir <dellspartak0@gmail.com>
2022-03-30 23:03:53 +0500

    add ijtimoiy

--- neftuyushma/src/components/ijtimoiy/ijtimoiy.jsx
+++ neftuyushma/src/components/ijtimoiy/ijtimoiy.jsx
@@ -8,24 +8,30 @@ const ijtimoiy = () => {
     <>
       <ul className="ijtimoiy_card">
         <li className="tel_item">
-          <div className="ijtimoiy_haed_tel">
-            <img src={tel} alt="icon" />
-            <h3 className="ijtimoiy_titel_tel"></h3>
+          <div className="ijtimoiy_haed_tel card_head">
+            <img className="tel_img" src={tel} alt="icon" />
+            <h3 className="ijtimoiy_titel_tel">Telefon raqam:</h3>
           </div>
           <div className="texts_ij">
-            <p className="number">+998 91 234 56 78</p>
+            <p className="number">(+998-99) 729-99-97</p>
           </div>
         </li>
         <li className="email_item">
-          <div className="ijtimoiy_haed_email">
-            <img src={emailimg} alt="icon" />
-            <h3 className="ijtimoiy_titel_email"></h3>
+          <div className="ijtimoiy_haed_email card_head">
+            <img className="email_img" src={emailimg} alt="icon" />
+            <h3 className="ijtimoiy_titel_email">E-mail</h3>
+          </div>
+          <div className="texts_ij">
+            <p className="email">nglab_info@mail.ru</p>
           </div>
         </li>
         <li className="tlg_item">
-          <div className="ijtimoiy_haed_tlg">
-            <img src={tlg} alt="icon" />
-            <h3 className="ijtimoiy_titel_tlg"></h3>
+          <div className="ijtimoiy_haed_tlg card_head">
+            <img className="tlg_img" src={tlg} alt="icon" />
+            <h3 className="ijtimoiy_titel_tlg">Telegram</h3>
+          </div>
+          <div className="texts_ij">
+            <p className="telegram">Savol berish</p>
           </div>
         </li>
       </ul>
