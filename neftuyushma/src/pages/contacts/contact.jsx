7549e25c9dd445bfdf91d5c763c1eadc6ce1b2be
Spartak-javohir <dellspartak0@gmail.com>
2022-03-30 07:09:13 +0500

    change

--- neftuyushma/src/pages/contacts/contact.jsx
+++ neftuyushma/src/pages/contacts/contact.jsx
@@ -2,14 +2,14 @@ import "./contact.css";
 import Navbar from "../../components/navbar/navbar";
 import Map from "../../components/maps/map";
 import Infocont from "../../components/infocont/infocont";
-import Rekvizits from "../../components/rekvizits/rekvizits";
+import Infoarr from "../../rout/infoarr";
 const contact = () => {
   return (
     <>
       <Navbar />
       <Infocont />
       <Map />
-      <Rekvizits />
+      <Infoarr />
     </>
   );
 };
