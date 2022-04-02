dbb94eb12436ac4072ac13850805bb5c534d0e81
Spartak-javohir <dellspartak0@gmail.com>
2022-04-02 22:47:17 +0500

    change

--- neftuyushma/src/components/navbar/navbar.jsx
+++ neftuyushma/src/components/navbar/navbar.jsx
@@ -12,21 +12,44 @@ const nav = () => {
           <Link to="/info">
             <img className="logotip" src={logo} alt="logo" />
           </Link>
-          <Link to="/info">Markaz haqida</Link>
-          <Link to="#">
-            Tadqiqot <br /> laboratoriyasi
-          </Link>
-          <Link to="/login"> Online kurslar</Link>
-          <Link to="#">
-            Neft va gaz <br /> uyushmasi
-          </Link>
-          <Link to="#">
-            Neft va gaz <br /> kutubxonasi
-          </Link>
-          <Link to="#">
-            Ilmiy tex <br /> jurnal
-          </Link>
-          <Link to="/contact"> Kontaktlar </Link>
+          {/* <select name="" id="">
+            <option value="">
+              <Link to={"#"}>Markaz haqida</Link>
+            </option>
+            <option value="">
+              <Link to={"#"}>Markaz nizomi</Link>
+            </option>
+            <option value="">
+              <Link to={"#"}>Rahbariyat</Link>
+            </option>
+            <option value="">
+              <Link to={"#"}>Maslahatchilar</Link>
+            </option>
+          </select> */}
+
+          <div className=" drList1">
+            <p>
+              Umumiy ma’lumotlar
+              <div className="drItem1">
+                <Link to={"#"}>Markaz haqida</Link>
+
+                <Link to={"#"}>Markaz nizomi</Link>
+
+                <Link to={"#"}>Rahbariyat</Link>
+
+                <Link to={"#"}>Maslahatchilar</Link>
+                <Link to={"#"}>Xodimlar</Link>
+                <Link to={"#"}>Aloqa</Link>
+              </div>
+            </p>
+          </div>
+
+          <Link to="/info">Umumiy ma’lumotlar</Link>
+          <Link to="#">Ilmiy-tadqiqot laboratoriyasi</Link>
+          <Link to="/login"> Ilmiy-texnikaviy jurnal</Link>
+          <Link to="#">Onlayn o’quv kurslari</Link>
+          <Link to="#">Neft-gaz elektron kutubxonasi</Link>
+
           <Burger />
         </ul>
       </div>
