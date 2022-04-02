a6c02a66859b87bbfae796e3633c267b62869391
Spartak-javohir <dellspartak0@gmail.com>
2022-04-02 23:16:36 +0500

    change

--- neftuyushma/src/components/navbar/navbar.jsx
+++ neftuyushma/src/components/navbar/navbar.jsx
@@ -12,36 +12,19 @@ const nav = () => {
           <Link to="/info">
             <img className="logotip" src={logo} alt="logo" />
           </Link>
-          {/* <select name="" id="">
-            <option value="">
+          <div className="drListOne">
+            <p>Umumiy ma’lumotlar</p>
+            <div className="drItemOne">
               <Link to={"#"}>Markaz haqida</Link>
-            </option>
-            <option value="">
+
               <Link to={"#"}>Markaz nizomi</Link>
-            </option>
-            <option value="">
+
               <Link to={"#"}>Rahbariyat</Link>
-            </option>
-            <option value="">
+
               <Link to={"#"}>Maslahatchilar</Link>
-            </option>
-          </select> */}
-
-          <div className=" drList1">
-            <p>
-              Umumiy ma’lumotlar
-              <div className="drItem1">
-                <Link to={"#"}>Markaz haqida</Link>
-
-                <Link to={"#"}>Markaz nizomi</Link>
-
-                <Link to={"#"}>Rahbariyat</Link>
-
-                <Link to={"#"}>Maslahatchilar</Link>
-                <Link to={"#"}>Xodimlar</Link>
-                <Link to={"#"}>Aloqa</Link>
-              </div>
-            </p>
+              <Link to={"#"}>Xodimlar</Link>
+              <Link to={"#"}>Aloqa</Link>
+            </div>
           </div>
 
           <Link to="/info">Umumiy ma’lumotlar</Link>
