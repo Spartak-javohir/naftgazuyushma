5ddeb9773b23a528070948a39b55bad7e15264c1
Spartak-javohir <dellspartak0@gmail.com>
2022-04-05 15:50:36 +0500

    change

--- neftuyushma/src/components/cardElem/card.jsx
+++ neftuyushma/src/components/cardElem/card.jsx
@@ -9,6 +9,12 @@ const cardElem = (props) => {
             <img src={props.img} alt="img" />
             <div className="card_text_div">
               <p className="card_text_div_texts">{props.text}</p>
+              <p className="name">{props.name}</p>
+              <p className="phoneNum">{props.num}</p>
+              <p className="mail">{props.mail}</p>
+              <p className="web">{props.web}</p>
+              <p className="qk">{props.qk}</p>
+              <p className="qv">{props.qv}</p>
             </div>
           </div>
         </div>
--- neftuyushma/src/pages/rahbariyat/rahbariyat.jsx
+++ neftuyushma/src/pages/rahbariyat/rahbariyat.jsx
@@ -3,6 +3,9 @@ import Card from "../../components/cardElem/card";
 import Navbar from "../../components/navbar/navbar";
 import img from "./img/rek.jpg";
 import pul from "./img/pul.png";
+import oyb from "./img/oyb.png";
+import eld from "./img/eld.jpg";
+
 const rahbar = () => {
   return (
     <>
@@ -10,7 +13,8 @@ const rahbar = () => {
         <Navbar />
         <Card img={img} text="salom" />
         <Card img={pul} text="salom" className="right" />
-        <Card text="salom" />
+        <Card img={oyb} />
+        <Card img={eld} className="right" />
       </section>
     </>
   );
