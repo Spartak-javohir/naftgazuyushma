70a13e6561ea3467491c4edb98003634ee0d2f45
Spartak-javohir <dellspartak0@gmail.com>
2022-04-05 10:27:28 +0500

    change

--- neftuyushma/src/components/cardElem/card.jsx
+++ neftuyushma/src/components/cardElem/card.jsx
@@ -3,7 +3,12 @@ import "./card.css";
 const cardElem = (props) => {
   return (
     <>
-      <div className="crad_div">{props}</div>
+      <div className="crad_div">
+        <img src={props.img} alt="img" />
+        <div className="card_text_div">
+          <p className="rank">{props.runk}</p>
+        </div>
+      </div>
     </>
   );
 };
