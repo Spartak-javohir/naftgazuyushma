36dcb8c53a8c084991a9579fc5374e5e70bc974c
Spartak-javohir <dellspartak0@gmail.com>
2022-04-06 13:34:18 +0500

    change

--- neftuyushma/src/pages/rahbariyat/rahbariyat.jsx
+++ neftuyushma/src/pages/rahbariyat/rahbariyat.jsx
@@ -31,8 +31,26 @@ const rahbar = () => {
           qv="Qabul qilish vaqti: Soat 15:00-17:00"
           className="right"
         />
-        <Card img={oyb} />
-        <Card img={eld} className="right" />
+        <Card
+          img={oyb}
+          text="Kafedra mudiri"
+          name="Aripdjanov Oybek Yusupdjanovich"
+          num="Tel: +998977744122"
+          mail="E-mail: Oybek_396@mail.ru"
+          qk="Qabul kunlari:   Seshanba,Payshanba"
+          qv="Qabul qilish vaqti: 14:00-16:00"
+        />
+        <Card
+          img={eld}
+          text="Markaz direktori"
+          name="Mashayev Eldor Ergashvoy o’g’li"
+          num="Tel: +998933922272"
+          mail="E-mail: eldor.mashayev@mail.ru"
+          web="Web site: www.dreldor.uz"
+          qk="Qabul qilish kuni: seshanba, payshanba"
+          qv="Qabul qilish vaqti: soat 15:00-17:00"
+          className="right"
+        />
       </section>
     </>
   );
