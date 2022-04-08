76e56598695c7274460f7f6bdf07a785800ec62b
Spartak-javohir <dellspartak0@gmail.com>
2022-04-08 18:55:37 +0500

    change

--- neftuyushma/src/components/el1/liElem.jsx
+++ neftuyushma/src/components/el1/liElem.jsx
@@ -1,4 +1,15 @@
 import "./liElem.css";
-const liElem = () => {
-  return <li></li>;
+const liElem = (props) => {
+  return (
+    <li className="ElemLi">
+      <img className="Ele" src={props.img} alt="photo" />
+      <div className="ElemLi_div">
+        <p className="ElemLi_div_name"></p>
+        <p className="ElemLi_div_position"></p>
+        <p className="ElemLi_div_0ne"></p>
+        <p className="ElemLi_div_two"></p>
+        <p className="ElemLi_div_three"></p>
+      </div>
+    </li>
+  );
 };
