e89a6da926051b39382fed5f4027d7c41cc4ade3
Spartak-javohir <dellspartak0@gmail.com>
2022-04-08 19:12:35 +0500

    add carusel

--- neftuyushma/src/components/el1/liElem.jsx
+++ neftuyushma/src/components/el1/liElem.jsx
@@ -4,12 +4,14 @@ const liElem = (props) => {
     <li className="ElemLi">
       <img className="Ele" src={props.img} alt="photo" />
       <div className="ElemLi_div">
-        <p className="ElemLi_div_name"></p>
-        <p className="ElemLi_div_position"></p>
-        <p className="ElemLi_div_0ne"></p>
-        <p className="ElemLi_div_two"></p>
-        <p className="ElemLi_div_three"></p>
+        <p className="EleLi_div_p ElemLi_div_name"></p>
+        <p className="EleLi_div_p ElemLi_div_position"></p>
+        <p className="EleLi_div_p ElemLi_div_0ne"></p>
+        <p className="EleLi_div_p ElemLi_div_two"></p>
+        <p className="EleLi_div_p ElemLi_div_three"></p>
       </div>
     </li>
   );
 };
+
+export default liElem;
--- neftuyushma/src/pages/consultants/consultants.jsx
+++ neftuyushma/src/pages/consultants/consultants.jsx
@@ -1,11 +1,38 @@
 import "./consultants.css";
 import Nav from "../../components/navbar/navbar";
+import LiElem from "../../components/el1/liElem";
 const consultants = () => {
   return (
     <>
       <Nav />
       <section className="consultants_section">
-        <div className="container"></div>
+        <div className="container">
+          <div className="consultants_section_div_one">
+            <ul className="consultants_section_list">
+              <LiElem />
+              <LiElem />
+              <LiElem />
+              <LiElem />
+            </ul>
+          </div>
+          <div className="consultants_section_div_two">
+            <ul className="consultants_section_list">
+              <LiElem />
+              <LiElem />
+              <LiElem />
+              <LiElem />
+            </ul>
+          </div>
+          <div className="consultants_section_div_three">
+            <ul className="consultants_section_list">
+              <LiElem />
+              <LiElem />
+              <LiElem />
+              <LiElem />
+            </ul>
+          </div>
+          <ul className="consultants_section_list"></ul>
+        </div>
       </section>
     </>
   );
