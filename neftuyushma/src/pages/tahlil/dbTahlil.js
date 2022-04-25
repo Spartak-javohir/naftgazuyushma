e6072fa9eac4590af4df2fc7d465307ef9ddff65
Spartak-javohir <dellspartak0@gmail.com>
2022-04-25 13:23:29 +0500

    add img

---
+++ neftuyushma/src/pages/tahlil/dbTahlil.js
@@ -0,0 +1,161 @@
+import DMA from "./img/DMA.png";
+import Areometr from "./img/areometr.png";
+import Arn from "./img/arn.png";
+import Arnlab from "./img/arnlab.png";
+import Astm from "./img/astm.png";
+import Chaqnash from "./img/chaqnash.png";
+import Aina from "./img/dina.png";
+import Englar from "./img/englar.png";
+import Formula from "./img/formula.png";
+import Haake from "./img/haake.png";
+import Koehlar from "./img/koehlar.png";
+import Spek from "./img/spek.png";
+import Vpj from "./img/vpj.png";
+import Yopiqtigel from "./img/yopiqtigel.png";
+
+export default dbTahlil = [
+  {
+    id: 1,
+    name: "200oC da DMA 1001 ixcham zichlik o'lchagich bilan yengil neft va neft mahsulotlarining zichligini aniqlash GOST R 57037-2016",
+    number: 2,
+    time: "5+4+5",
+    summ: 14,
+    price: 499530.6,
+    pictures: { DMA },
+  },
+  {
+    id: 2,
+    name: "",
+    number: 0,
+    time: 1,
+    summ: 1,
+    price: 12,
+    pictures: "",
+  },
+  {
+    id: 3,
+    name: "",
+    number: 0,
+    time: 1,
+    summ: 1,
+    price: 12,
+    pictures: "",
+  },
+  {
+    id: 4,
+    name: "",
+    number: 0,
+    time: 1,
+    summ: 1,
+    price: 12,
+    pictures: "",
+  },
+  {
+    id: 5,
+    name: "",
+    number: 0,
+    time: 1,
+    summ: 1,
+    price: 12,
+    pictures: "",
+  },
+  {
+    id: 6,
+    name: "",
+    number: 0,
+    time: 1,
+    summ: 1,
+    price: 12,
+    pictures: "",
+  },
+  {
+    id: 7,
+    name: "",
+    number: 0,
+    time: 1,
+    summ: 1,
+    price: 12,
+    pictures: "",
+  },
+  {
+    id: 8,
+    name: "",
+    number: 0,
+    time: 1,
+    summ: 1,
+    price: 12,
+    pictures: "",
+  },
+  {
+    id: 9,
+    name: "",
+    number: 0,
+    time: 1,
+    summ: 1,
+    price: 12,
+    pictures: "",
+  },
+  {
+    id: 10,
+    name: "",
+    number: 0,
+    time: 1,
+    summ: 1,
+    price: 12,
+    pictures: "",
+  },
+  {
+    id: 11,
+    name: "",
+    number: 0,
+    time: 1,
+    summ: 1,
+    price: 12,
+    pictures: "",
+  },
+  {
+    id: 12,
+    name: "",
+    number: 0,
+    time: 1,
+    summ: 1,
+    price: 12,
+    pictures: "",
+  },
+  {
+    id: 13,
+    name: "",
+    number: 0,
+    time: 1,
+    summ: 1,
+    price: 12,
+    pictures: "",
+  },
+  {
+    id: 14,
+    name: "",
+    number: 0,
+    time: 1,
+    summ: 1,
+    price: 12,
+    pictures: "",
+  },
+  {
+    id: 15,
+    name: "",
+    number: 0,
+    time: 1,
+    summ: 1,
+    price: 12,
+    pictures: "",
+  },
+  {
+    id: 16,
+    name: "",
+    number: 0,
+    time: 1,
+    summ: 1,
+    price: 12,
+    pictures: "",
+  },
+];
