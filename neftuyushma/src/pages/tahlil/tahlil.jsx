421971365edabcc8647e1ac5a4748b31f8cf8e48
Spartak-javohir <dellspartak0@gmail.com>
2022-04-25 12:45:58 +0500

    add baza

--- neftuyushma/src/pages/tahlil/tahlil.jsx
+++ neftuyushma/src/pages/tahlil/tahlil.jsx
@@ -5,8 +5,189 @@ const tahlil = () => {
   return (
     <>
       <Nav />
-      <section>
-        <div className="container"></div>
+      <section className="section_tahlil">
+        <div className="container">
+          <header className="header_tahlil">
+            <h2>
+              Tahlil turlari, neft va neft mahsulotlarining bir namunasining
+              xossalarini aniqlash uchun vaqt normasi va narxi
+            </h2>
+          </header>
+          <table>
+            <thead>
+              <tr>
+                <th>№</th>
+                <th>Tahlil nomi</th>
+                <th>Tahlillar soni</th>
+                <th>1 tahlil uchun vaqt normasi (daq)</th>
+                <th>Jami (daq)</th>
+                <th>Tahlillarning umumiy summasi (QQSsiz) (so’m)</th>
+                <th>Apparat rasmi</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>1</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <th>2</th>
+
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <th>3</th>
+
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <th>4</th>
+
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <th>5</th>
+
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <th>6</th>
+
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <th>7</th>
+
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <th>8</th>
+
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <th>9</th>
+
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <th>10</th>
+
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <th>11</th>
+
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <th>12</th>
+
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <th>13</th>
+
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <th>14</th>
+
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <th>15</th>
+
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <th>16</th>
+
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </section>
     </>
   );
