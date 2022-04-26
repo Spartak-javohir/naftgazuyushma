685ba6ded4491e598b5ee4dc4fe608b46c109122
Spartak-javohir <dellspartak0@gmail.com>
2022-04-26 11:41:32 +0500

    add  drobdavn

--- neftuyushma/src/components/drobdawn/drobdawn.jsx
+++ neftuyushma/src/components/drobdawn/drobdawn.jsx
@@ -1,49 +1,195 @@
-import React, { useState } from "react";
-import { MenuItems } from "../menuItem/MenuItems";
+// import React, { useState } from "react";
+// import { MenuItems } from "../menuItem/MenuItems";
 import "./drobdawn.css";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+
+// function Dropdown() {
+//   const [click, setClick] = useState(false);
+
+//   const handleClick = () => setClick(!click);
+
+//   return (
+//     <>
+//       <ul
+//         onClick={handleClick}
+//         className={click ? "dropdown-menu clicked" : "dropdown-menu"}
+//       >
+//         <li key={1}>
+//           <Link
+//             className="dropdown-link"
+//             to="/list"
+//             onClick={() => setClick(false)}
+//           >
+//             drobdawn
+//           </Link>
+//           <Link to={"/info"}>Markaz haqida</Link>
+//         </li>
+//         <li key={2}>
+//           <Link
+//             className="dropdown-link"
+//             to="/list"
+//             onClick={() => setClick(false)}
+//           >
+//             qqsk
+//           </Link>
+//         </li>
+//         <li key={3}>
+//           <Link
+//             className="dropdown-link"
+//             to="/list"
+//             onClick={() => setClick(false)}
+//           >
+//             qqsk
+//           </Link>
+//         </li>
+//       </ul>
+//     </>
+//   );
+// }
+
+// export default Dropdown;
+
+// import "./index.css";
+import { ReactComponent as BellIcon } from "./icons/bell.svg";
+import { ReactComponent as MessengerIcon } from "./icons/messenger.svg";
+import { ReactComponent as CaretIcon } from "./icons/caret.svg";
+import { ReactComponent as PlusIcon } from "./icons/plus.svg";
+import { ReactComponent as CogIcon } from "./icons/cog.svg";
+import { ReactComponent as ChevronIcon } from "./icons/chevron.svg";
+import { ReactComponent as ArrowIcon } from "./icons/arrow.svg";
+import { ReactComponent as BoltIcon } from "./icons/bolt.svg";
+
+import React, { useState, useEffect, useRef } from "react";
+import { CSSTransition } from "react-transition-group";
 
 function Dropdown() {
-  const [click, setClick] = useState(false);
+  return (
+    <Navbar>
+      {/* <NavItem icon={<PlusIcon />} />
+      <NavItem icon={<BellIcon />} />
+      <NavItem icon={<MessengerIcon />} /> */}
 
-  const handleClick = () => setClick(!click);
+      <NavItem icon={<CaretIcon />}>
+        <DropdownMenu></DropdownMenu>
+      </NavItem>
+    </Navbar>
+  );
+}
+
+function Navbar(props) {
+  return (
+    <nav className="navbar_list">
+      <ul className="navbar-nav">{props.children}</ul>
+    </nav>
+  );
+}
+
+function NavItem(props) {
+  const [open, setOpen] = useState(false);
 
   return (
-    <>
-      <ul
-        onClick={handleClick}
-        className={click ? "dropdown-menu clicked" : "dropdown-menu"}
+    <li className="nav-item">
+      <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
+        {props.icon}
+      </a>
+
+      {open && props.children}
+    </li>
+  );
+}
+
+function DropdownMenu() {
+  const [activeMenu, setActiveMenu] = useState("main");
+  const [menuHeight, setMenuHeight] = useState(null);
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    setMenuHeight(dropdownRef.current?.firstChild.offsetHeight);
+  }, []);
+
+  function calcHeight(el) {
+    const height = el.offsetHeight;
+    setMenuHeight(height);
+  }
+
+  function DropdownItem(props) {
+    return (
+      <a
+        href="#"
+        className="menu-item"
+        onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}
       >
-        <li key={1}>
-          <Link
-            className="dropdown-link"
-            to="/list"
-            onClick={() => setClick(false)}
+        <span className="icon-button">{props.leftIcon}</span>
+        {props.children}
+        <span className="icon-right">{props.rightIcon}</span>
+      </a>
+    );
+  }
+
+  return (
+    <div className="dropdown" style={{ height: menuHeight }} ref={dropdownRef}>
+      <CSSTransition
+        in={activeMenu === "main"}
+        timeout={500}
+        classNames="menu-primary"
+        unmountOnExit
+        onEnter={calcHeight}
+      >
+        <div className="menu">
+          <DropdownItem>My Profile</DropdownItem>
+          <DropdownItem
+            leftIcon={<CogIcon />}
+            rightIcon={<ChevronIcon />}
+            goToMenu="settings"
           >
-            drobdawn
-          </Link>
-          <Link to={"/info"}>Markaz haqida</Link>
-        </li>
-        <li key={2}>
-          <Link
-            className="dropdown-link"
-            to="/list"
-            onClick={() => setClick(false)}
+            Settings
+          </DropdownItem>
+          <DropdownItem
+            leftIcon="🦧"
+            rightIcon={<ChevronIcon />}
+            goToMenu="animals"
           >
-            qqsk
-          </Link>
-        </li>
-        <li key={3}>
-          <Link
-            className="dropdown-link"
-            to="/list"
-            onClick={() => setClick(false)}
-          >
-            qqsk
-          </Link>
-        </li>
-      </ul>
-    </>
+            Animals
+          </DropdownItem>
+        </div>
+      </CSSTransition>
+
+      <CSSTransition
+        in={activeMenu === "settings"}
+        timeout={500}
+        classNames="menu-secondary"
+        unmountOnExit
+        onEnter={calcHeight}
+      >
+        <div className="menu">
+          <DropdownItem goToMenu="main" leftIcon={<ArrowIcon />}>
+            <h2>My Tutorial</h2>
+          </DropdownItem>
+          <DropdownItem leftIcon={<BoltIcon />}>HTML</DropdownItem>
+          <DropdownItem leftIcon={<BoltIcon />}>CSS</DropdownItem>
+          <DropdownItem leftIcon={<BoltIcon />}>JavaScript</DropdownItem>
+          <DropdownItem leftIcon={<BoltIcon />}>Awesome!</DropdownItem>
+        </div>
+      </CSSTransition>
+
+      <CSSTransition
+        in={activeMenu === "animals"}
+        timeout={500}
+        classNames="menu-secondary"
+        unmountOnExit
+        onEnter={calcHeight}
+      >
+        <div className="menu">
+          <DropdownItem goToMenu="main" leftIcon={<ArrowIcon />}>
+            <h2>Animals</h2>
+          </DropdownItem>
+          <DropdownItem leftIcon="🦘">Kangaroo</DropdownItem>
+          <DropdownItem leftIcon="🐸">Frog</DropdownItem>
+          <DropdownItem leftIcon="🦋">Horse?</DropdownItem>
+          <DropdownItem leftIcon="🦔">Hedgehog</DropdownItem>
+        </div>
+      </CSSTransition>
+    </div>
   );
 }
 
