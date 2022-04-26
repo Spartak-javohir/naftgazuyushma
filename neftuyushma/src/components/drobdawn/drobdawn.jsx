2a3b0c8f7e0ab6f51fd33f93f32430ef6272d09b
Spartak-javohir <dellspartak0@gmail.com>
2022-04-26 16:23:18 +0500

    change style dropdown

--- neftuyushma/src/components/drobdawn/drobdawn.jsx
+++ neftuyushma/src/components/drobdawn/drobdawn.jsx
@@ -57,19 +57,19 @@ function DropdownMenu() {
     setMenuHeight(height);
   }
 
-  function DropdownItem(props) {
-    return (
-      <a
-        href="#"
-        className="menu-item"
-        onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}
-      >
-        <span className="icon-button">{props.leftIcon}</span>
-        {props.children}
-        <span className="icon-right">{props.rightIcon}</span>
-      </a>
-    );
-  }
+  // function DropdownItem(props) {
+  //   return (
+  //     <a
+  //       href="#"
+  //       className="menu-item"
+  //       onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}
+  //     >
+  //       <span className="icon-button">{props.leftIcon}</span>
+  //       {props.children}
+  //       <span className="icon-right">{props.rightIcon}</span>
+  //     </a>
+  //   );
+  // }
 
   return (
     <div className="dropdown" style={{ height: menuHeight }} ref={dropdownRef}>
@@ -88,42 +88,6 @@ function DropdownMenu() {
           <Link to={"/consultants"}>Maslahatchilar</Link>
           <Link to={"/staff"}>Xodimlar</Link>
           <Link to={"/contact"}>Aloqa</Link>
-        </div>
-      </CSSTransition>
-
-      <CSSTransition
-        in={activeMenu === "settings"}
-        timeout={500}
-        classNames="menu-secondary"
-        unmountOnExit
-        onEnter={calcHeight}
-      >
-        <div className="menu">
-          <DropdownItem goToMenu="main" leftIcon={<ArrowIcon />}>
-            <h2>My Tutorial</h2>
-          </DropdownItem>
-          <DropdownItem leftIcon={<BoltIcon />}>HTML</DropdownItem>
-          <DropdownItem leftIcon={<BoltIcon />}>CSS</DropdownItem>
-          <DropdownItem leftIcon={<BoltIcon />}>JavaScript</DropdownItem>
-          <DropdownItem leftIcon={<BoltIcon />}>Awesome!</DropdownItem>
-        </div>
-      </CSSTransition>
-
-      <CSSTransition
-        in={activeMenu === "animals"}
-        timeout={500}
-        classNames="menu-secondary"
-        unmountOnExit
-        onEnter={calcHeight}
-      >
-        <div className="menu">
-          <DropdownItem goToMenu="main" leftIcon={<ArrowIcon />}>
-            <h2>Animals</h2>
-          </DropdownItem>
-          <DropdownItem leftIcon="🦘">Kangaroo</DropdownItem>
-          <DropdownItem leftIcon="🐸">Frog</DropdownItem>
-          <DropdownItem leftIcon="🦋">Horse?</DropdownItem>
-          <DropdownItem leftIcon="🦔">Hedgehog</DropdownItem>
         </div>
       </CSSTransition>
     </div>
