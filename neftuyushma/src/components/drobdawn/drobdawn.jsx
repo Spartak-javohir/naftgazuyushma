52511196aee09ef2f6887bb49f26d42136b3e777
Spartak-javohir <dellspartak0@gmail.com>
2022-04-29 23:53:14 +0500

    add dropdawn

--- neftuyushma/src/components/drobdawn/drobdawn.jsx
+++ neftuyushma/src/components/drobdawn/drobdawn.jsx
@@ -8,8 +8,10 @@ const { render } = ReactDOM;
 const DropDownMenu = () => {
   const [open, setOpen] = useState(false);
   const [openone, setOpenone] = useState(false);
+  const [opentwo, setOpentwo] = useState(false);
   const container = useRef(null);
   const containerone = useRef(null);
+  const containertwo = useRef(null);
 
   const handleClickOutside = (event) => {
     if (container.current && !container.current.contains(event.target)) {
@@ -20,6 +22,11 @@ const DropDownMenu = () => {
   const handleClickOutsideone = (event) => {
     if (containerone.current && !containerone.current.contains(event.target)) {
       setOpenone(false);
+    }
+  };
+  const handleClickOutsidetwo = (event) => {
+    if (containertwo.current && !containertwo.current.contains(event.target)) {
+      setOpentwo(false);
     }
   };
 
@@ -38,6 +45,15 @@ const DropDownMenu = () => {
     return () => {
       // clean up
       document.removeEventListener("mousedown", handleClickOutsideone);
+    };
+  });
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutsidetwo);
+
+    return () => {
+      // clean up
+      document.removeEventListener("mousedown", handleClickOutsidetwo);
     };
   });
 
@@ -77,22 +93,59 @@ const DropDownMenu = () => {
         </button>
         {openone && (
           <ul className="dropdown-wrapperone dropdown-menuone">
-            <Link to={"/laboratoryInfo"} className="dropdown-menu__itemone">
+            <Link to={"/laboratoryInfo"} className="dropdown-menuone__itemone">
               Laboratoriya haqida
             </Link>
 
-            <Link to={"/tahlil"} className="dropdown-menu__itemone">
+            <Link to={"/tahlil"} className="dropdown-menuone__itemone">
               Tahlil xizmatlari
             </Link>
 
-            <Link to={"#"} className="dropdown-menu__itemone">
+            <Link to={"#"} className="dropdown-menuone__itemone">
               Jihozlar
             </Link>
 
-            <Link to={"#"} className="dropdown-menu__itemone">
+            <Link to={"#"} className="dropdown-menuone__itemone">
               Buyurtma qoldirish
             </Link>
-            <Link to={"#"} className="dropdown-menu__itemone">
+            <Link to={"#"} className="dropdown-menuone__itemone">
+              Bog'lanish
+            </Link>
+          </ul>
+        )}
+      </div>
+      <div className="dropdown_containertwo" ref={handleClickOutsidetwo}>
+        <button
+          type="button"
+          className="buttontwo"
+          onClick={() => setOpentwo(!opentwo)}
+        >
+          Ilmiy-texnikaviy jurnal
+        </button>
+        {opentwo && (
+          <ul className="dropdown-wrappertwo dropdown-menutwo">
+            <Link to={"#"} className="dropdown-menutwo__itemtwo">
+              Jurnal haqida
+            </Link>
+
+            <Link to={"#"} className="dropdown-menutwo__itemtwo">
+              Jurnal nizomi
+            </Link>
+
+            <Link to={"#"} className="dropdown-menutwo__itemtwo">
+              Bosh muharrir
+            </Link>
+
+            <Link to={"#"} className="dropdown-menutwo__itemtwo">
+              Tahrir hay'ati
+            </Link>
+            <Link to={"#"} className="dropdown-menutwo__itemtwo">
+              Arxiv
+            </Link>
+            <Link to={"#"} className="dropdown-menutwo__itemtwo">
+              Maqola yuborish
+            </Link>
+            <Link to={"#"} className="dropdown-menutwo__itemtwo">
               Bog'lanish
             </Link>
           </ul>
@@ -101,11 +154,5 @@ const DropDownMenu = () => {
     </section>
   );
 };
-
-// const App = () => (
-//   <div className="App">
-//     <DropDownMenu />
-//   </div>
-// );
 
 export default DropDownMenu;
