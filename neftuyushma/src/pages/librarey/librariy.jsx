9c6a0c1222559c68a68e10da7c8aca409bc6d0fc
Spartak-javohir <dellspartak0@gmail.com>
2022-05-06 14:43:19 +0500

    frontend full

--- neftuyushma/src/pages/librarey/librariy.jsx
+++ neftuyushma/src/pages/librarey/librariy.jsx
@@ -1,9 +1,41 @@
+import { Link } from "react-router-dom";
 import Nav from "../../components/navbar/navbar";
-
+import "./librariy.css";
+import file from "./file/file.pdf";
 const library = () => {
   return (
     <>
       <Nav />
+      <section className="library_section">
+        <div className="container">
+          <ul className="library_list">
+            <li className="library_item">
+              <Link to={file} download>
+                file download
+              </Link>
+            </li>
+            <li className="library_item">file download</li>
+            <li className="library_item">file download</li>
+            <li className="library_item">file download</li>
+            <li className="library_item">file download</li>
+            <li className="library_item">file download</li>
+            <li className="library_item">file download</li>
+            <li className="library_item">file download</li>
+            <li className="library_item">file download</li>
+            <li className="library_item">file download</li>
+            <li className="library_item">file download</li>
+            <li className="library_item">file download</li>
+            <li className="library_item">file download</li>
+            <li className="library_item">file download</li>
+            <li className="library_item">file download</li>
+            <li className="library_item">file download</li>
+            <li className="library_item">file download</li>
+            <li className="library_item">file download</li>
+            <li className="library_item">file download</li>
+            <li className="library_item">file download</li>
+          </ul>
+        </div>
+      </section>
     </>
   );
 };
