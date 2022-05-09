18b0cdf636c049848700d5aebc4a5ac3c401806e
Spartak-javohir <dellspartak0@gmail.com>
2022-05-09 23:38:11 +0500

    change

--- neftuyushma/src/App.jsx
+++ neftuyushma/src/App.jsx
@@ -1,10 +1,3 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { BrowserRouter as Router, Route } from "react-router-dom";
-
-// import Login from "./components/login/login";
-// import Home from "./pages/home";
-// import Singup from "./components/singup/singup";
-
 import PublicRoute from "./routs/PublicRoute";
 const App = () => {
   return (
