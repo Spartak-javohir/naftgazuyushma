fc9fb671748503916279043d5126f509bff4ff1c
Spartak-javohir <dellspartak0@gmail.com>
2022-05-09 23:52:31 +0500

    change

--- neftuyushma/src/servise/routs/ProtectedRoute.jsx
+++ neftuyushma/src/servise/routs/ProtectedRoute.jsx
@@ -2,13 +2,13 @@ import { UseAuth } from "../context/AuthContext";
 import { Route, Redirect } from "react-router-dom";
 
 export default function ProtectedRoute(props) {
-  // const [token, setToken] = UseAuth();
-  // if (!token) {
-  //   return (
-  //     <Routes>
-  //       <Route path="/" element={<Navigate to="/" />} />;
-  //     </Routes>
-  //   );
-  // }
-  // return <Route {...props} />;
+  const [token, setToken] = UseAuth();
+  if (!token) {
+    return (
+      <Routes>
+        <Route path="/" element={<Navigate to="/" />} />;
+      </Routes>
+    );
+  }
+  return <Route {...props} />;
 }
